26545fc274df01442163b825af4e30b6008b6873
Andrew Hinojosa <drews_view@hotmail.com>
2020-02-14 03:44:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN"
-  elseif phrase == phrase
+  else if phrase == phrase
     "HUH?! SPEAK UP, SONNY!"
   else phrase != phrase
     "NO, NOT SINCE 1938!"
